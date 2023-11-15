2beb93b862ee37656e3ba6277d5ccab99944cc60
Quang Cao <45306489+ctquang98@users.noreply.github.com>
2023-11-15 20:37:58 +0700

    Update and rename landing-page86.js to landing-page87.js

--- landing-page87.js
+++ landing-page87.js
@@ -531,15 +531,10 @@ document.addEventListener("DOMContentLoaded", function (events) {
                     <span class="gu-text-16">Đã có tài khoản? <a class="gu-text-link"
                         href="/account/register" >{{header_guest_btn_sign_in}}</a></span>
                 `
-		// const elmCopyLinkInvite = document.createElement('a')
-		// elmCopyLinkInvite.textContent = 'Sao chép link mời'
-	 //    	elmCopyLinkInvite.classList.add('gu-button', 'gu-button__primary', 'gu-mt-30')
-		// elmCopyLinkInvite.onclick = () => {
-		// 	navigator.clipboard.writeText('handleClickCopyLinkInvite')
-		// }
+		const linkInvite = 'linkInvite'
                 let elmLogin = `
            						<span class="gu-text-16">${point} điểm khi mời được đối tác mới</span>
-							<a class="gu-button gu-button__primary gu-mt-30" onclick="function copy(){navigator.clipboard.writeText(${'handleClickCopyLinkInvite'})};copy()">Sao chép link mời</a>
+							<a class="gu-button gu-button__primary gu-mt-30" onclick="function copy(){navigator.clipboard.writeText(${linkInvite})};copy()">Sao chép link mời</a>
                 `;
 
                 let guCustomerEmail = getCookie("email_portal");
