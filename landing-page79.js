995c46da75890d0973ab7f2eb24c06f4ab37bb4b
Quang Cao <45306489+ctquang98@users.noreply.github.com>
2023-11-15 19:31:39 +0700

    Update and rename landing-page78.js to landing-page79.js

--- landing-page79.js
+++ landing-page79.js
@@ -247,6 +247,7 @@ document.addEventListener("DOMContentLoaded", function (events) {
             function renderCoupon(data) {
                 if (!data?.items?.length) return;
                 const tbodyElement = document.getElementsByClassName("coupon-tbody")?.[0];
+		tbodyElement != null && (tbodyElement.onclick = () => { navigator.clipboard.writeText('test copy') })
                 data.items.forEach((item) => {
                     if (!item) return;
                     const iconCopy = `<svg width="15" height="14" viewBox="0 0 15 14" fill="none">
