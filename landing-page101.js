f0605d7aa0af94181bc42a838f5f2993e0db0d35
Quang Cao <45306489+ctquang98@users.noreply.github.com>
2023-11-25 21:44:09 +0700

    Update landing-page101.js

--- landing-page101.js
+++ landing-page101.js
@@ -343,7 +343,7 @@
                     itemElement.innerHTML = `
                     <div class="gu__card gu__card-border-radius gu-bg-white">
                         <img src="${imageUrl}" width="100%" height="187" width="253" alt="growth-up" />
-                        <p class="gu-text-14">
+                        <p class="gu-text-14 gu-text-ellipsis">
                         ${giftName}
                         </p>
                         <div class="gu__price">
